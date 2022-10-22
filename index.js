ae50ab68e871e15715e979325f585b0e2e82cbb3
Fatema04 <shadrinmoni15@gmail.com>
2022-10-21 21:19:35 -0700

    all  fldr added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = 5000
 const cors = require('cors')
 const categories = require('./data/categories.json')
+const news = require('./data/news.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -12,6 +13,9 @@ app.use(cors())
 
 app.get('/categories', (req, res) => {
     res.send(categories)
+})
+app.get('/news', (req, res) => {
+    res.send(news)
 })
 
 app.listen(port, () => {
