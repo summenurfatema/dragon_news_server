bbe6b4bdc2791f91f4cecca3d0a9fc0699a3e4cc
Fatema04 <shadrinmoni15@gmail.com>
2022-10-21 22:30:58 -0700

    news added

--- index.js
+++ index.js
@@ -15,6 +15,10 @@ app.get('/categories', (req, res) => {
     res.send(categories)
 })
 
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
 app.get('/category/:id', (req, res) => {
     const id = req.params.id;
     if (id === "08") {
